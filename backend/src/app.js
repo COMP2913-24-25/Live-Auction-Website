2033acb88fb782d51fe7cbd9c79ba96ee1e2c415
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-02-12 17:14:03 +0000

    implemented route for fetching active auctions

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,4 +15,21 @@ app.get('/', (req, res) => {
   res.send('Hello from the backend!');
 });
 
+// Fetch all active auctions
+app.get('/api/auctions', (req, res) => {
+  const sql = `
+    SELECT items.*, users.username AS seller_name
+    FROM items
+    JOIN users ON items.user_id = users.id
+    WHERE end_time > datetime('now')
+    ORDER BY created_at DESC`
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(rows);
+  });
+})
+
 module.exports = app;
