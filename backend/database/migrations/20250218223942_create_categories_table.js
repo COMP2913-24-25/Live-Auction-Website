ff1dcbd6165d273ff7b6f41c4a99edf7be6d9518
zq1120 <zhiqinggg1120@gmail.com>
2025-02-18 22:53:44 +0000

    migrated new categories table

---
+++ backend/database/migrations/20250218223942_create_categories_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('categories', function(table) {
+        table.increments('id').primary();
+        table.string('name').notNullable().unique();
+        table.text('description');
+        table.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('categories');
+};
+  
