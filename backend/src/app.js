2d9018dd93b4073c0b73b2a4bbc051eb491236dc
zq1120 <zhiqinggg1120@gmail.com>
2025-02-19 13:34:24 +0000

    fixed imported routing

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,8 +40,8 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api', uploadRoutes);
 app.use('/api/auctions', auctionRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/categories', categoriesRoutes);
-app.use('/api/search', searchRoutes);
+app.use('/api/', categoriesRoutes);
+app.use('/api/', searchRoutes);
 
 // Example route
 app.get('/', (req, res) => {
