29fe1812e9b51733dd586842cfbdcbcd9c832b72
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-02-25 16:18:00 +0000

    applied Pascal case to ProtectedRoute

--- frontend/src/context/ProtectedRoute.jsx
+++ frontend/src/context/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from './AuthContext';
 import PropTypes from 'prop-types';
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
