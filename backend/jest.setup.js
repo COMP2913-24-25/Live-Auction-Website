0535755cb75d6ce357e1cc55b6f011ae7d7661e5
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-03-27 00:16:44 +0000

    added global fetch mock

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -6,6 +6,13 @@ const db = knex({
   connection: { filename: ':memory:' }, // Use in-memory DB for testing
 });
 
+// Mock global fetch to avoid network tests
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  })
+);
+
 beforeEach(async () => {
   await db.migrate.latest(); // Run migrations before each test
   await db.seed.run(); // Optionally run seeds
