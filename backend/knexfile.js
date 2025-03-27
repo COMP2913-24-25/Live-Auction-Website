0538e3354eb01350433ab6480a3623822820c073
TexHaotiantan <tanhaotian66@gmail.com>
2025-03-27 21:05:42 +0000

    Replace PostgreSQL with SQLite

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -21,34 +21,40 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      filename: './database/staging.sqlite'
     },
+    useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: './database/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      filename: './database/production.sqlite'
     },
+    useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: './database/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
     }
   }
 
